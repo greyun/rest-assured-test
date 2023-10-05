71683b14f523989ac65e3c65b48fefd8427b6b28
윤현영 <greyun@woowahan.com>
2023-10-05 19:48:34 +0900

    initialize spring project

---
+++ rest-assured/src/test/java/com/greyun/restassured/RestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greyun.restassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
