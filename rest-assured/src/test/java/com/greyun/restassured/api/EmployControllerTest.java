8695181a2e4ced2f3b7d65b61a467482b9bb868e
윤현영 <greyun@woowahan.com>
2023-10-05 21:26:25 +0900

    add mockMvc test

--- rest-assured/src/test/java/com/greyun/restassured/api/EmployControllerTest.java
+++ rest-assured/src/test/java/com/greyun/restassured/api/EmployControllerTest.java
@@ -5,29 +5,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
 class EmployControllerTest {
     private EmployService employService;
+    private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
         employService = mock(EmployService.class);
-    }
+        mockMvc = MockMvcBuilders.standaloneSetup(new EmployController(employService))
+                .build();
 
-    @Test
-    void getEmploys() {
         BDDMockito.given(employService.getEmploys())
                 .willReturn(List.of(
                         new Employ("greyun", 1234L),
                         new Employ("pablo", 5678L)
                 ));
+    }
 
+    @Test
+    void getEmploys() {
         given().standaloneSetup(new EmployController(employService))
                 .param("name", "greyun")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -37,5 +45,17 @@ class EmployControllerTest {
                 .get("/employs")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    void getEmploys_mockMvc() throws Exception {
+        mockMvc.perform(
+                get("/employs")
+                        .param("name", "greyun")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"greyun\",\"age\":30}"))
+                .andDo(print())
+                .andExpect(jsonPath("$[0].name").value("greyun"))
+                .andExpect(jsonPath("$[0].departmentId").value(1234L));
     }
 }
